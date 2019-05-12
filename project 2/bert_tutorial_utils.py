eb15634616a7643b7b845fe489746549ed548052
Metod Jazbec <jazbec.metod@gmail.com>
2019-05-12 12:31:05 +0200

    initial project 2

---
+++ project 2/bert_tutorial_utils.py
@@ -0,0 +1,16 @@
+# data preprocessing for BERT colab tutorial: https://colab.research.google.com/github/google-research/bert/blob/master/predicting_movie_reviews_with_bert_on_tf_hub.ipynb
+
+import pandas as pd
+
+def data_to_bert_tutorial(data_path):
+  our_data = pd.read_csv(data_path)
+  our_data['4_sentences'] = our_data[["InputSentence1","InputSentence2","InputSentence3","InputSentence4"]].apply(lambda x: ' '.join(x), axis=1)
+  processed_data = pd.DataFrame(columns=['story', 'label'])
+  for _, row in our_data.iterrows():
+    sample1 = {'story': row["4_sentences"] + " " + row["RandomFifthSentenceQuiz1"], 'label': 1 if row['AnswerRightEnding']==1 else 0}
+    sample2 = {'story': row["4_sentences"] + " " + row["RandomFifthSentenceQuiz2"], 'label': 1 if row['AnswerRightEnding']==2 else 0}
+    processed_data = processed_data.append(sample1, ignore_index=True)
+    processed_data = processed_data.append(sample2, ignore_index=True)
+
+  processed_data['label'] = processed_data['label'].astype(int)
+  return processed_data
