1bf788b7ceff844bdf27cd6e2e35a8a8eb2da8bc
Metod Jazbec <jazbec.metod@gmail.com>
2019-05-19 20:26:13 +0200

    segment embeddings A,B

--- project 2/bert_tutorial_utils.py
+++ project 2/bert_tutorial_utils.py
@@ -5,10 +5,10 @@ import pandas as pd
 def data_to_bert_tutorial(data_path):
   our_data = pd.read_csv(data_path)
   our_data['4_sentences'] = our_data[["InputSentence1","InputSentence2","InputSentence3","InputSentence4"]].apply(lambda x: ' '.join(x), axis=1)
-  processed_data = pd.DataFrame(columns=['story', 'label'])
+  processed_data = pd.DataFrame(columns=['story', 'ending', 'label'])
   for _, row in our_data.iterrows():
-    sample1 = {'story': row["4_sentences"] + " " + row["RandomFifthSentenceQuiz1"], 'label': 1 if row['AnswerRightEnding']==1 else 0}
-    sample2 = {'story': row["4_sentences"] + " " + row["RandomFifthSentenceQuiz2"], 'label': 1 if row['AnswerRightEnding']==2 else 0}
+    sample1 = {'story': row["4_sentences"], 'ending': row["RandomFifthSentenceQuiz1"], 'label': 1 if row['AnswerRightEnding']==1 else 0}
+    sample2 = {'story': row["4_sentences"], 'ending': row["RandomFifthSentenceQuiz2"], 'label': 1 if row['AnswerRightEnding']==2 else 0}
     processed_data = processed_data.append(sample1, ignore_index=True)
     processed_data = processed_data.append(sample2, ignore_index=True)
 
