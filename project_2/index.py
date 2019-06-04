c21fe62be80a356ca1c57ce18edab7545a73baee
Emanuele Palumbo <e.palumbo.17@gmail.com>
2019-06-04 15:35:30 +0200

    updating sentiment

--- project_2/index.py
+++ project_2/index.py
@@ -11,6 +11,14 @@ import csv
 import utils as utils
 from model import Model
 
+
+# train stories are 88161
+# total train batches are 11021
+# validation stories are 1871
+# total validation batches are 234
+
+
+
 #GRAPH
 
 tf.reset_default_graph()
@@ -18,13 +26,14 @@ tf.reset_default_graph()
 
 model = Model(initializer=tf.contrib.layers.xavier_initializer(),
               stories_file='data/train_stories.csv',
+              validation_file='data/cloze_test_val__spring2016 - cloze_test_ALL_val.csv',
               embedding_dim=3,
               state_dim=64,
               stories_dim=5,
               learning_rate=0.001,
               batch_size=8,
               max_grad_norm=5.0,
-              num_epochs=2)
+              num_epochs=1)
 
 
 #SESSION
@@ -33,28 +42,145 @@ with tf.Session() as session:
 
     session.run(tf.global_variables_initializer())
 
-    session.run(model.iterator_op)
 
-    count_batches = 0
+    with open('experiments/exp_final_1.txt', 'w') as result_file:
+
+        train_sent_batches = 8817
+
+        for i in range(model.num_epochs):
+
+            session.run(model.iterator_op)
+
+            count_batches = 0
 
 
-    with open('experiments/experiment4.txt', 'w') as result_file:
+            result_file.write('\n\n'+'START TRAINING SENTIMENT PART EPOCH '+str(i+1)+'\n\n')
 
+            # TRAINING THE SENTIMENT MODEL ON PART OF TRAINING DATASET
+
+            while True:
+
+                try:
+
+                    b_loss, _, g_step = session.run([ model.loss, model.optimize_op, model.global_step])
+
+                    count_batches += 1
+
+                    if count_batches % 300 == 0:
+
+                        result_file.write('Processed batches: '+ str(count_batches)+'\n'+
+                                          'Batch loss: '+str(b_loss)+'\n'+
+                                          'Global step: '+ str(g_step)+'\n')
+
+                    if count_batches == train_sent_batches:
+                        break
+
+                except tf.errors.OutOfRangeError:
+                    break
+
+
+            # TESTING THE SENTIMENT MODEL ON PART OF TRAINING DATASET
+
+            result_file.write('\n\n'+'START TESTING SENTIMENT PART EPOCH ' + str(i+1) + '\n\n')
+
+            while True:
+
+                try:
+                    b_loss = session.run([model.loss])
+
+                    count_batches += 1
+
+                    if count_batches % 300 == 0:
+
+                        result_file.write('Processed batches: ' + str(count_batches-train_sent_batches) + '\n' +
+                                          'Batch loss: ' + str(b_loss) + '\n' +
+                                          'Global step: ' + str(g_step) + '\n')
+
+                except tf.errors.OutOfRangeError:
+                    break
+        
+        result_file.write('\n\n'+'STARTING TRAINING STORY CLOZE'+'\n\n')
+
+        session.run(model.iterator_val_op)
+
+        count_sc_batches = 0
+
+        train_sc_batches = 187
 
         while True:
 
             try:
 
-                c_s_shape, b_loss, _, g_step = session.run([ model.cosine_sim_shape, model.loss, model.optimize_op, model.global_step])
+                b_loss_sc, _ , g_step = session.run([model.loss_sc, model.optimize_sc_op, model.global_step])
 
-                count_batches+=1
+                count_sc_batches += 1
 
-                result_file.write('Processed batches: '+ str(count_batches)+'\n'+
-                                  'Cosine_sim_shape: '+ str(c_s_shape)+'\n'+
-                                  'Batch loss: '+str(b_loss)+'\n'+
-                                  'Global step: '+ str(g_step)+'\n')
-                if g_step % 200 == 0:
-                    print(b_loss)
+                if count_sc_batches % 10:
+
+                    result_file.write('Processed batches: ' + str(count_sc_batches) + '\n' +\
+                                      'Batch loss: ' + str(b_loss_sc) + '\n' +\
+                                      'Global step: ' + str(g_step) + '\n')
+
+                if count_sc_batches == train_sc_batches:
+                    break
 
             except tf.errors.OutOfRangeError:
                 break
+        
+        result_file.write('\n\n' + 'STARTING TESTING STORY CLOZE' + '\n\n')
+
+        '''
+        while True:
+
+            try:
+
+                acc = session.run([model.accuracy])
+
+                count_sc_batches += 1
+
+                result_file.write(str(acc)+'\n')
+
+            except tf.errors.OutOfRangeError:
+                break
+        '''
+
+
+
+
+
+#   THIS PART IS TO HAVE THE TXT FILE WITH THE PREDICTED AND THE VADER SENTIMENT EMBEDDINGS
+
+
+    with open('experiments/s_files/s_file_final_1.txt', 'w') as p_file:
+        
+        session.run(model.iterator_val_op)
+        
+        
+        while True:
+
+            try:
+
+                ep, ee1, ee2 = session.run([model.e_p_batch, model.e1_e_batch, model. e2_e_batch])
+
+                for i in range(model.batch_size):
+
+                    try:
+
+                        p_file.write(str(ep[i]) +' '+ str(ee1[i]) +'\n')
+                        p_file.write(str(ep[i]) +' '+ str(ee2[i]) +'\n')
+
+
+                    except IndexError:
+                        break
+
+            except tf.errors.OutOfRangeError:
+                break
+
+
+
+
+
+
+
+
+
--- project_2/model.py
+++ project_2/model.py
@@ -11,12 +11,9 @@ import csv
 import utils as utils
 
 
-
-
-
 class Model:
 
-    def __init__(self,initializer,stories_file, embedding_dim=3, state_dim = 64, stories_dim = 5,  learning_rate=0.001, batch_size=8, max_grad_norm=5.0,
+    def __init__(self,initializer,stories_file, validation_file, embedding_dim=3, state_dim = 64, stories_dim = 5,  learning_rate=0.001, batch_size=8, max_grad_norm=5.0,
                  num_epochs=1):
 
 
@@ -38,28 +35,43 @@ class Model:
 
 
 
-        # Input file
+        # Input files
         self.stories_file = stories_file
+        self.validation_file = validation_file
 
 
-
-        with tf.name_scope("dataset_initialization"):
+        with tf.name_scope("dataset_initialization_training"):
 
             dataset = tf.data.Dataset.from_tensor_slices(utils.read_embed_createtensor_from_file_stories(self.stories_file)).map(utils.embedding_io_map)\
-                .repeat(self.num_epochs)\
                 .batch(self.batch_size)
             iterator = tf.data.Iterator.from_structure(dataset.output_types, dataset.output_shapes)
+
+            #print(dataset.output_types, dataset.output_shapes)
 
             self.x_batch, self.y_batch = iterator.get_next()
 
             self.iterator_op = iterator.make_initializer(dataset)
 
 
+        with tf.name_scope('dataset_initialization_validation'):
+
+            valid_dataset = tf.data.Dataset.from_tensor_slices(utils.read_embed_createtensor_from_stories_val(self.validation_file)).map(utils.embedding_io_map_val)\
+                .batch(self.batch_size)
+            iterator_val = tf.data.Iterator.from_structure(valid_dataset.output_types, valid_dataset.output_shapes)
+
+            #print(valid_dataset.output_types, valid_dataset.output_shapes)
+
+            self.s_batch, self.e1_e_batch, self.e2_e_batch, self.ans_batch = iterator_val.get_next()
+
+            self.iterator_val_op = iterator_val.make_initializer(valid_dataset)
+
         # Weights
         with tf.name_scope("weights_initialization"):
 
-            self.output_weights = tf.get_variable("output_weights",shape=[self.state_dim, self.embedding_dim], initializer=self.initializer, trainable=True)
+            self.output_weights = tf.get_variable("output_weights",shape=[self.state_dim,self.embedding_dim], initializer=self.initializer, trainable=True)
             self.output_bias = tf.get_variable("output_bias",shape=self.embedding_dim, initializer=self.initializer, trainable=True)
+
+            self.similarity_matrix = tf.get_variable("similarity_matrix",shape=[self.embedding_dim, self.embedding_dim],initializer=self.initializer, trainable=True)
 
 
         with tf.name_scope('lstm'):
@@ -76,34 +88,81 @@ class Model:
 
             lstm_output, (state_c,state_h) = tf.nn.dynamic_rnn(cell=self.LSTM, inputs=self.x_batch, dtype=tf.float32)
 
-            logits = tf.nn.softmax(tf.matmul(state_h, self.output_weights, name="output_multiplication")+self.output_bias)
+            logits_sent = tf.nn.softmax(tf.matmul(state_h,self.output_weights, name="output_multiplication")+self.output_bias)
 
 
 
         with tf.name_scope('cosine_sim_and_loss'):
 
-            self.cosine_sim, self.cosine_sim_shape  = utils.cosine_similarity(logits, self.y_batch)
+            self.cosine_sim = utils.cosine_similarity(logits_sent, self.y_batch)
 
             self.loss = -(tf.reduce_mean(self.cosine_sim))
 
 
+        with tf.name_scope('fine_tuning_story_cloze'):
+
+            lstm_output_sc, (state_c_sc, state_h_sc) = tf.nn.dynamic_rnn(cell=self.LSTM, inputs=self.s_batch, dtype=tf.float32)
+
+            self.e_p_batch = tf.nn.softmax(tf.matmul(state_h_sc, self.output_weights) + self.output_bias)
+
+            logits_1 = tf.linalg.diag_part(tf.matmul(tf.matmul(a=self.e_p_batch, b=self.similarity_matrix),self.e1_e_batch, transpose_b=True))
+
+            logits_2 = tf.linalg.diag_part(tf.matmul(tf.matmul(a=self.e_p_batch, b=self.similarity_matrix),self.e2_e_batch, transpose_b=True))
+
+            self.logits_sc = tf.stack([logits_1,logits_2], axis = 1)
+
+            '''
+            print(logits_1.shape, logits_2.shape, self.logits_sc.shape)
+            print(self.ans_batch.shape)
+            print(tf.one_hot(indices=self.ans_batch, depth=2))
+            '''
+            # self.one_hot_ans_batch = tf.one_hot(indices=self.ans_batch, depth=2)
+
+            loss_sc_batch = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.ans_batch, logits=self.logits_sc, name = 'sparse_cross_entropy_softmax_sc')
+
+            self.loss_sc = tf.reduce_mean(loss_sc_batch)
+
+        '''
+
+        with tf.name_scope('accuracy'):
+
+            self.predictions = tf.argmax(self.logits_sc, axis=1)
+
+            #print(self.predictions.shape, self.ans_batch.shape)
+
+            self.accuracy = tf.metrics.accuracy(labels=self.ans_batch, predictions=self.predictions)
+
+        '''
 
 
-        with tf.name_scope('train'):
+        with tf.name_scope('optimization'):
 
-            self.optimize_op = self.train()
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+            optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, clip_norm=self.max_grad_norm)
+            self.optimize_op = optimizer.minimize(self.loss, global_step=self.global_step)
+
+            self.optimize_sc_op = optimizer.minimize(self.loss_sc, global_step=self.global_step)
 
 
 
+'''
 
-    def train(self):
-        with tf.name_scope("optimization"):
+    def train_sentiment_model(self):
+        with tf.name_scope("optimization_sentiment_model"):
             optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
             optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, clip_norm=self.max_grad_norm)
             optimize_op = optimizer.minimize(self.loss, global_step=self.global_step)
         return optimize_op
 
+    def train_story_cloze(self):
+        with tf.name_scope("optimization_story_cloze"):
+            optimizer_sc = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+            optimizer_sc = tf.contrib.estimator.clip_gradients_by_norm(optimizer_sc, clip_norm=self.max_grad_norm)
+            optimize_sc_op = optimizer_sc.minimize(self.loss_sc, global_step=self.global_step)
+        return optimize_sc_op
 
+
+'''
 
 
 
