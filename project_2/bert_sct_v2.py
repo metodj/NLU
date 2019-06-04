1390596450ac502d71bea6de559e7315faf756ca
roksikonja <rsikonja@student.ethz.ch>
2019-06-04 14:05:28 +0200

    data_pp + data_pp_test (74 %), model from hub/check, cs_dist + sentiment

---
+++ project_2/bert_sct_v2.py
@@ -0,0 +1,186 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tensorflow as tf
+from bert import tokenization
+
+from bert_sct_utils_v2 import create_tokenizer_from_hub_module, SctProcessor, get_config
+from bert_sct_utils_v2 import model_fn_builder, file_based_input_fn_builder, file_based_convert_examples_to_features
+
+flags = tf.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("data_dir", None, "The input data dir. Should contain the .tsv files (or other data files).")
+flags.DEFINE_string("output_dir", None, "The output directory where the model checkpoints will be written.")
+flags.DEFINE_string("results_dir", "./results/", "The output directory for result files.")
+
+# BERT
+flags.DEFINE_string("bert_model_hub", None, "Pretrained BERT. (default)")
+flags.DEFINE_string("bert_dir", None, "Local directory of pretrained BERT.")
+flags.DEFINE_bool("bert_trainable", False, "Whether BERT weights are trainable.")
+flags.DEFINE_string("init_checkpoint", None, "Initial checkpoint of BERT.")
+
+# Training or evaluation
+flags.DEFINE_bool("do_train", False, "Whether to run training.")
+flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+flags.DEFINE_bool("do_predict", False, "Whether to run the model in inference mode on the test set.")
+
+flags.DEFINE_integer("max_seq_length", 400, "Max. length after tokenization. If shorter padded, else truncated.")
+flags.DEFINE_integer("batch_size", 4, "Total batch size for training.")
+flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+flags.DEFINE_float("num_train_epochs", 1.0, "Total number of training epochs to perform.")
+flags.DEFINE_float("warmup_proportion", 0.1, "Proportion of training to perform linear learning rate warmup for.")
+
+
+def main(_):
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    if not(FLAGS.bert_model_hub or (FLAGS.bert_dir and FLAGS.init_checkpoint)):
+        raise ValueError("bert_model_hub or (bert_config_file and init_checkpoint)")
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Create output directory
+    tf.gfile.MakeDirs(FLAGS.output_dir)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Processor
+    processor = SctProcessor()
+    label_list = processor.get_labels()
+
+    bert_config = None
+    if not FLAGS.init_checkpoint:
+        tokenizer = create_tokenizer_from_hub_module(FLAGS.bert_model_hub)
+
+    else:
+        tokenizer = tokenization.FullTokenizer(vocab_file=os.path.join(FLAGS.bert_dir, "vocab.txt"),
+                                               do_lower_case=False)
+        bert_config = get_config(os.path.join(FLAGS.bert_dir, "bert_config.json"))
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Model and Estimator
+    train_examples = None
+    num_train_steps = None
+    num_warmup_steps = None
+    if FLAGS.do_train:
+        train_examples = processor.get_train_examples(FLAGS.data_dir)
+        num_train_steps = int(len(train_examples) / FLAGS.batch_size * FLAGS.num_train_epochs)
+        num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
+
+    run_config = tf.estimator.RunConfig(model_dir=FLAGS.output_dir,
+                                        save_summary_steps=10,
+                                        save_checkpoints_steps=1000)
+
+    model_fn = model_fn_builder(bert_model_hub=FLAGS.bert_model_hub,
+                                bert_trainable=FLAGS.bert_trainable,
+                                bert_config=bert_config,
+                                init_checkpoint=FLAGS.init_checkpoint,
+                                num_labels=len(label_list),
+                                learning_rate=FLAGS.learning_rate,
+                                num_train_steps=num_train_steps,
+                                num_warmup_steps=num_warmup_steps)
+
+    params = {
+        "batch_size": FLAGS.batch_size,
+    }
+
+    estimator = tf.estimator.Estimator(model_fn=model_fn,
+                                       config=run_config,
+                                       params=params)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Training
+    if FLAGS.do_train:
+        train_file = os.path.join(FLAGS.data_dir, "train.tf_record")
+        file_based_convert_examples_to_features(train_examples,
+                                                label_list,
+                                                FLAGS.max_seq_length,
+                                                tokenizer,
+                                                train_file)
+
+        tf.logging.info("***** Running training *****")
+        tf.logging.info("  Num examples = %d", len(train_examples))
+        tf.logging.info("  Batch size = %d", FLAGS.batch_size)
+        tf.logging.info("  Num steps = %d", num_train_steps)
+
+        train_input_fn = file_based_input_fn_builder(input_file=train_file,
+                                                     seq_length=FLAGS.max_seq_length,
+                                                     is_training=True,
+                                                     drop_remainder=False)
+
+        estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Evaluation
+    if FLAGS.do_eval:
+        eval_examples = processor.get_dev_examples(FLAGS.data_dir)
+
+        eval_file = os.path.join(FLAGS.data_dir, "eval.tf_record")
+        file_based_convert_examples_to_features(eval_examples,
+                                                label_list,
+                                                FLAGS.max_seq_length,
+                                                tokenizer,
+                                                eval_file)
+
+        tf.logging.info("***** Running evaluation *****")
+        tf.logging.info("  Num examples = %d", len(eval_examples))
+
+        eval_steps = int(len(eval_examples) // FLAGS.batch_size)
+
+        eval_input_fn = file_based_input_fn_builder(input_file=eval_file,
+                                                    seq_length=FLAGS.max_seq_length,
+                                                    is_training=False,
+                                                    drop_remainder=False)
+
+        result = estimator.evaluate(input_fn=eval_input_fn, steps=eval_steps)
+
+        output_eval_file = os.path.join(FLAGS.results_dir, "eval_results.txt")
+        with tf.gfile.GFile(output_eval_file, "w") as writer:
+            tf.logging.info("***** Eval results *****")
+            for key in sorted(result.keys()):
+                tf.logging.info("  %s = %s", key, str(result[key]))
+                writer.write("%s = %s\n" % (key, str(result[key])))
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # Test
+    if FLAGS.do_predict:
+        predict_examples = processor.get_test_examples(FLAGS.data_dir)
+
+        predict_file = os.path.join(FLAGS.data_dir, "predict.tf_record")
+        file_based_convert_examples_to_features(predict_examples,
+                                                label_list,
+                                                FLAGS.max_seq_length,
+                                                tokenizer,
+                                                predict_file)
+
+        tf.logging.info("***** Running prediction*****")
+        tf.logging.info("  Num examples = %d", len(predict_examples))
+
+        predict_input_fn = file_based_input_fn_builder(input_file=predict_file,
+                                                       seq_length=FLAGS.max_seq_length,
+                                                       is_training=False,
+                                                       drop_remainder=False)
+
+        result = estimator.predict(input_fn=predict_input_fn)
+
+        output_predict_file = os.path.join(FLAGS.results_dir, "test_results.tsv")
+        with tf.gfile.GFile(output_predict_file, "w") as writer:
+            num_written_lines = 0
+            tf.logging.info("***** Predict results *****")
+            for (i, prediction) in enumerate(result):
+                probabilities = prediction["probabilities"]
+                predict_label = str(prediction["predict_labels"])
+                target_label = str(prediction["target_labels"])
+
+                probs = ["{0:.4f}".format(class_probability) for class_probability in probabilities]
+
+                output_line = "\t".join(probs) + "\t" + target_label + "\t" + predict_label + "\n"
+                writer.write(output_line)
+                num_written_lines += 1
+
+
+if __name__ == "__main__":
+    flags.mark_flag_as_required("data_dir")
+    flags.mark_flag_as_required("output_dir")
+    tf.app.run()
---
+++ project_2/preprocess_data_v2.py
@@ -0,0 +1,165 @@
+import os
+import pandas as pd
+import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+def pp_data_train(df, method="random", sentiment=True, common_sense=True):
+    df = df.copy()
+    cols = list(df.columns)
+
+    def pp_text_a(row):
+        text_a = ""
+        text_a = text_a + " " + row[cols.index("sentence1")]  # sentence1
+        text_a = text_a + " " + row[cols.index("sentence2")]  # sentence2
+        text_a = text_a + " " + row[cols.index("sentence3")]  # sentence3
+        text_a = text_a + " " + row[cols.index("sentence4")]  # sentence4
+        return text_a
+
+    def pp_text_b_pos(row):
+        text_b = row[cols.index("sentence5")]
+        return text_b
+
+    def pp_sentiment(row):
+        sentiment = analyser.polarity_scores(row)
+        np_vec = np.array([sentiment["compound"], sentiment["neg"], sentiment["neu"], sentiment["pos"]],
+                          dtype=np.float32)
+        return np_vec
+
+    def pp_common_sense(row):
+        return np.ones(shape=(4,), dtype=np.float32)
+
+    # Positive samples (true)
+    df_pos = pd.DataFrame()
+    df_pos["unique_id"] = df["storyid"]
+    df_pos["label"] = 1  # "positive"
+
+    df_pos["text_a"] = df.apply(pp_text_a, axis=1)
+    df_pos["text_b_pos"] = df.apply(pp_text_b_pos, axis=1)
+
+    if method == "random":
+        df_pos["text_b_neg"] = df["sentence3"].sample(frac=1.0, random_state=0).reset_index(drop=True)
+
+    # Sentiment
+    if sentiment:
+        df_pos["vs_sent1"] = df["sentence1"].apply(pp_sentiment)
+        df_pos["vs_sent2"] = df["sentence2"].apply(pp_sentiment)
+        df_pos["vs_sent3"] = df["sentence3"].apply(pp_sentiment)
+        df_pos["vs_sent4"] = df["sentence4"].apply(pp_sentiment)
+        df_pos["vs_sent5_pos"] = df_pos["text_b_pos"].apply(pp_sentiment)
+        df_pos["vs_sent5_neg"] = df_pos["text_b_neg"].apply(pp_sentiment)
+
+    # Common Sense
+    if common_sense:
+        df_pos["cs_dist_pos"] = df_pos["text_a"].apply(pp_common_sense)
+        df_pos["cs_dist_neg"] = -df_pos["text_a"].apply(pp_common_sense)
+
+    # Sort by index: positive - first, negative - second
+    df_res = df_pos.set_index(["unique_id", "label"])\
+        .sort_index(axis=0, ascending=False).reset_index(drop=False)
+
+    return df_res
+
+
+def pp_data_val(df, sentiment=True, common_sense=True):
+    df = df.copy()
+    cols = list(df.columns)
+
+    def pp_text_a(row):
+        text_a = ""
+        text_a = text_a + " " + row[cols.index("InputSentence1")]  # sentence1
+        text_a = text_a + " " + row[cols.index("InputSentence2")]  # sentence2
+        text_a = text_a + " " + row[cols.index("InputSentence3")]  # sentence3
+        text_a = text_a + " " + row[cols.index("InputSentence4")]  # sentence4
+        return text_a
+
+    def pp_text_b_pos(row):
+        if row[cols.index("AnswerRightEnding")] == 1:
+            return row[cols.index("RandomFifthSentenceQuiz1")]
+        elif row[cols.index("AnswerRightEnding")] == 2:
+            return row[cols.index("RandomFifthSentenceQuiz2")]
+
+    def pp_text_b_neg(row):
+        if row[cols.index("AnswerRightEnding")] == 2:
+            return row[cols.index("RandomFifthSentenceQuiz1")]
+        elif row[cols.index("AnswerRightEnding")] == 1:
+            return row[cols.index("RandomFifthSentenceQuiz2")]
+
+    def pp_sentiment(row):
+        sentiment = analyser.polarity_scores(row)
+        np_vec = np.array([sentiment["compound"], sentiment["neg"], sentiment["neu"], sentiment["pos"]],
+                          dtype=np.float32)
+        return np_vec
+
+    def pp_common_sense(row):
+        return np.ones(shape=(4,), dtype=np.float32)
+
+    # Positive samples
+    df_pos = pd.DataFrame()
+    df_pos["unique_id"] = df["InputStoryid"]
+    df_pos["label"] = 1  # "positive"
+    df_pos["text_a"] = df.apply(pp_text_a, axis=1)
+
+    df_pos["text_b_pos"] = df.apply(pp_text_b_pos, axis=1)
+    df_pos["text_b_neg"] = df.apply(pp_text_b_neg, axis=1)
+
+    # Sentiment
+    if sentiment:
+        df_pos["vs_sent1"] = df["InputSentence1"].apply(pp_sentiment)
+        df_pos["vs_sent2"] = df["InputSentence2"].apply(pp_sentiment)
+        df_pos["vs_sent3"] = df["InputSentence3"].apply(pp_sentiment)
+        df_pos["vs_sent4"] = df["InputSentence4"].apply(pp_sentiment)
+        df_pos["vs_sent5_pos"] = df_pos["text_b_pos"].apply(pp_sentiment)
+        df_pos["vs_sent5_neg"] = df_pos["text_b_neg"].apply(pp_sentiment)
+
+    # Common Sense
+    if common_sense:
+        df_pos["cs_dist_pos"] = df_pos["text_a"].apply(pp_common_sense)
+        df_pos["cs_dist_neg"] = -df_pos["text_a"].apply(pp_common_sense)
+
+    df_res = df_pos.set_index(["unique_id", "label"])\
+        .sort_index(axis=0, ascending=False).reset_index(drop=False)
+
+    return df_res
+
+
+if __name__ == "__main__":
+    analyser = SentimentIntensityAnalyzer()
+
+    data_val = pd.read_csv(os.path.join("data", "cloze_test_val__spring2016 - cloze_test_ALL_val.csv"))
+    data_train = pd.read_csv(os.path.join("data", "train_stories.csv"))
+
+    # Create dataframes
+    df_train = pp_data_train(data_train, sentiment=True, common_sense=True)
+    df_val = pp_data_val(data_val, sentiment=True, common_sense=True)
+    df_test = df_val[["unique_id", "label", "text_a", "text_b_pos", "text_b_neg",
+                      "vs_sent1", "vs_sent2", "vs_sent3", "vs_sent4",
+                      "vs_sent5_pos", "cs_dist_pos", "vs_sent5_neg", "cs_dist_neg"
+                      ]].sample(frac=0.5, random_state=0).reset_index(drop=True)
+
+    # Save as .tsv
+    # if not os.path.exists("data_pp"):
+    #     os.makedirs("data_pp")
+    #
+    df_train.to_csv(os.path.join("data_pp", "sct_v2.train.tsv"), sep="\t", header=True, index=False)
+    df_val.to_csv(os.path.join("data_pp", "sct_v2.validation.tsv"), sep="\t", header=True, index=False)
+    df_test.to_csv(os.path.join("data_pp", "sct_v2.test.tsv"), sep="\t", header=True, index=False)
+
+    if not os.path.exists("data_pp_test"):
+        os.makedirs("data_pp_test")
+
+    df_val.iloc[0:1500].to_csv(os.path.join("data_pp_test", "sct_v2.train.tsv"), sep="\t", header=True, index=False)
+    df_val.iloc[1500:].to_csv(os.path.join("data_pp_test", "sct_v2.validation.tsv"), sep="\t", header=True, index=False)
+    df_test.to_csv(os.path.join("data_pp_test", "sct_v2.test.tsv"), sep="\t", header=True, index=False)
+
+    # max_seq_len
+    idx_a = list(df_train.columns).index("text_a")
+    idx_b_pos = list(df_train.columns).index("text_b_pos")
+    idx_b_neg = list(df_train.columns).index("text_b_neg")
+
+    print("train_len", df_train.apply(lambda row: len(row[idx_a]), axis=1).max() +
+          df_train.apply(lambda row: max(len(row[idx_b_pos]), len(row[idx_b_neg])), axis=1).max())
+    print("val_len", df_val.apply(lambda row: len(row[idx_a]), axis=1).max() +
+          df_val.apply(lambda row: max(len(row[idx_b_pos]), len(row[idx_b_neg])), axis=1).max())
+    print("test_len", df_test.apply(lambda row: len(row[idx_a]), axis=1).max() +
+          df_test.apply(lambda row: max(len(row[idx_b_pos]), len(row[idx_b_neg])), axis=1).max())
