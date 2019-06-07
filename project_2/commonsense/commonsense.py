e3f5779f03d4e55fa1fe98c96d2fb5c6d42e5c52
Björn Jarisch <Bjoern.Jarisch@ti8m.ch>
2019-06-07 10:33:51 +0200

    Add support for Cloze file format (id,s1,s2,s3,s4,e1,e2,solution)
    Note: The output contains two vectors for the two possible closing sentences. The order is NOT always right, wrong. The vectors are in the same order as in the input. The answer column (1/0) indicates which vector is for the right answer.

--- project_2/commonsense/commonsense.py
+++ project_2/commonsense/commonsense.py
@@ -1,4 +1,5 @@
 import tokenization
+import csv
 import re
 from scipy import spatial
 
@@ -6,9 +7,15 @@ numberbatch_vectors = []
 
 def run():
     bert_dir = "..\\..\\..\\bert-master\\data\\data_pp\\"
+    cloze_dir = "..\\..\\..\\"
     output_dir = "output\\data_pp\\"
-    input_file = bert_dir + "sct.test.tsv"
-    output_file = output_dir + "sct.test.results.tsv"
+
+    #input_file = bert_dir + "sct.test.tsv"
+    #output_file = output_dir + "sct.test.results.tsv"
+
+    input_file = cloze_dir + "cloze_test_val__spring2016 - cloze_test_ALL_val.csv"
+    output_file = output_dir + "cloze_test_val.csv"
+    cloze_format = True
 
     set_type = "test"
 
@@ -20,31 +27,46 @@ def run():
     numberbatch_vectors = read_numberbatch(numberbatch_file)
 
     # Expected input format: id \t label \t text_a (s1..s4) \t text_b (e) 
-    # TODO: Get input with non-concatenated sentences if possible
-    examples = read_examples(input_file, set_type)
+    # Expected input format for Cloze: id,s1,s2,s3,s4,e1,e2,answer
+    examples = read_examples(input_file, set_type, cloze_format)
 
-    results = [(guid, label, compute_distance(S, e)) for (guid, S, e, label) in examples]
-    write_results(results, output_file, set_type)
+    results = [(guid, label, [compute_distance(S, e) for e in E]) for (guid, S, E, label) in examples]
+    write_results(results, output_file, set_type, cloze_format)
 
 # File handling
 # -------------
 
-def read_examples(input_file, set_type):
+def read_examples(input_file, set_type, cloze_format = False):
     with open(input_file) as file:
-        lines = file.read().splitlines()
+        # Read either as Cloze-CSV or as TSV
+        delimiter = "," if cloze_format else "\t"
+        quotechar = "\"" if cloze_format else None
+
+        reader = csv.reader(file, delimiter=delimiter, quotechar=quotechar)
+        lines = []
+        for line in reader:
+            lines.append(line)
 
     examples = []
     for (i, line) in enumerate(lines):
         if i == 0:
             continue
 
-        line = line.split("\t")
-        # TODO: Tokenize, delete stop words using NLTK/CoreNLP
-        guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
-        S = split_on_punctuation(tokenization.convert_to_unicode(line[2]))[:4]
-        e = tokenization.convert_to_unicode(line[3])
-        label = tokenization.convert_to_unicode(line[1])
-        examples.append((guid, S, e, label))
+        if cloze_format:
+            # Parse Cloze format.
+            # TODO: Tokenize, delete stop words using NLTK/CoreNLP
+            guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
+            S = [tokenization.convert_to_unicode(l) for l in line[1:5]]
+            E = [tokenization.convert_to_unicode(l) for l in line[5:7]]
+            answer = line[7]
+            examples.append((guid, S, E, answer))
+        else:
+            # TODO: Tokenize, delete stop words using NLTK/CoreNLP
+            guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
+            S = split_on_punctuation(tokenization.convert_to_unicode(line[2]))[:4]
+            E = [tokenization.convert_to_unicode(line[3])]
+            label = tokenization.convert_to_unicode(line[1])
+            examples.append((guid, S, E, label))
     return examples
 
 def read_numberbatch(input_file):
@@ -60,11 +82,11 @@ def read_numberbatch(input_file):
             vectors[word] = vector
     return vectors
 
-def write_results(results, output_file, set_type):
+def write_results(results, output_file, set_type, cloze_format = False):
     with open(output_file, "w") as file:
-
+        delimiter = "," if cloze_format else "\t"
         for (guid, label, distance) in results:
-            line = guid + "\t" + label + "\t" + ", ".join(str(d) for d in distance) + "\n"
+            line = guid + delimiter + label + delimiter + ",".join(str(d) for d in distance) + "\n"
             file.write(line)
 
 # Tokenization
